343e13f18cad1e204fb1c60a52a4e6210fd2b25a
rominalodolo <lodoloromina@gmail.com>
2022-01-11 14:00:10 +0200

    update

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from '@firebase/firestore' 
+// import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCcG7BGPdKTRxcdK7TMpL0wkHviGU0aAlY",
@@ -12,4 +13,6 @@ const firebaseConfig = {
   };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const db = getFirestore()
+// const analytics = getAnalytics(app);
