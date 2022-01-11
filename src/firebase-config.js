4343edf7617f9e0b486ed29102d9fc4c70b28161
rominalodolo <lodoloromina@gmail.com>
2022-01-11 13:25:49 +0200

    create

---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCcG7BGPdKTRxcdK7TMpL0wkHviGU0aAlY",
+    authDomain: "fir-crud-react-3d90b.firebaseapp.com",
+    projectId: "fir-crud-react-3d90b",
+    storageBucket: "fir-crud-react-3d90b.appspot.com",
+    messagingSenderId: "206334012285",
+    appId: "1:206334012285:web:31078e6161b3ae667f7a14",
+    measurementId: "G-Y3VXVT835N"
+  };
