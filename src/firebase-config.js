ed6edc69ea202cc0f6a629ff1050cb6715e06460
rominalodolo <lodoloromina@gmail.com>
2022-01-11 13:27:45 +0200

    update

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,4 +1,5 @@
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCcG7BGPdKTRxcdK7TMpL0wkHviGU0aAlY",
