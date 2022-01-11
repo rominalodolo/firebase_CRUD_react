d25abd0b1541ae74cd8699fba03c54dbb33cb0f8
rominalodolo <lodoloromina@gmail.com>
2022-01-11 14:39:56 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import { useState, useEffect } from "react"; 
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 import { db } from "./firebase-config";
-import { collection, doc, getDocs, updateDoc, deleteDoc, addDoc } from "firebase/firestore"; 
+import {
+  collection,
+  doc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  addDoc,
+} from "firebase/firestore";
 
 function App() {
   const [newName, setNewName] = useState("");
   const [newAge, setNewAge] = useState(0);
 
   const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, "users"); 
+  const usersCollectionRef = collection(db, "users");
 
   const createUser = async () => {
     await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
@@ -24,12 +31,11 @@ function App() {
     const userDoc = doc(db, "users", id);
     await deleteDoc(userDoc);
   };
-  
+
   useEffect(() => {
     const getUsers = async () => {
       const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
-
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
 
     getUsers();
@@ -37,17 +43,47 @@ function App() {
 
   return (
     <div className="App">
-      {users.map ((user) => {
+      <input
+        placeholder="Name..."
+        onChange={(event) => {
+          setNewName(event.target.value);
+        }}
+      />
+      <input
+        type="number"
+        placeholder="Age..."
+        onChange={(event) => {
+          setNewAge(event.target.value);
+        }}
+      />
+
+      <button onClick={createUser}> Create User</button>
+      {users.map((user) => {
         return (
-        <div> 
-          {" "}
-          <h1>Name: {user.name} </h1>
-          <h1>Surame: {user.surname} </h1>
-          <h1>Age: {user.age} </h1>
-          <h1>Occupation: {user.occupation} </h1>
-        </div>
-      }
-      )}</div>
+          <div>
+            {" "}
+            <h1>Name: {user.name}</h1>
+            <h1>Age: {user.age}</h1>
+            <button
+              onClick={() => {
+                updateUser(user.id, user.age);
+              }}
+            >
+              {" "}
+              Increase Age
+            </button>
+            <button
+              onClick={() => {
+                deleteUser(user.id);
+              }}
+            >
+              {" "}
+              Delete User
+            </button>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,14 +4,14 @@ import { getFirestore } from "@firebase/firestore";
 
 // rather create an .env file and populate the strings in that file for safety
 const firebaseConfig = {
-    apiKey: "AIzaSyCcG7BGPdKTRxcdK7TMpL0wkHviGU0aAlY",
-    authDomain: "fir-crud-react-3d90b.firebaseapp.com",
-    projectId: "fir-crud-react-3d90b",
-    storageBucket: "fir-crud-react-3d90b.appspot.com",
-    messagingSenderId: "206334012285",
-    appId: "1:206334012285:web:31078e6161b3ae667f7a14",
-    measurementId: "G-Y3VXVT835N"
-  };
+  apiKey: "AIzaSyCcG7BGPdKTRxcdK7TMpL0wkHviGU0aAlY",
+  authDomain: "fir-crud-react-3d90b.firebaseapp.com",
+  projectId: "fir-crud-react-3d90b",
+  storageBucket: "fir-crud-react-3d90b.appspot.com",
+  messagingSenderId: "206334012285",
+  appId: "1:206334012285:web:31078e6161b3ae667f7a14",
+  measurementId: "G-Y3VXVT835N",
+};
 
 const app = initializeApp(firebaseConfig);
 
