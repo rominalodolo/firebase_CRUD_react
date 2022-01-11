d2860c973cf4a77c33400118ac68d74bfc1c21ae
rominalodolo <lodoloromina@gmail.com>
2022-01-11 14:20:59 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react"; 
 import './App.css';
+import { db } from ".firebase-config";
+import { collection } from "firebase/firestore"; 
 
 function App() {
-  const [user, setUsers] = useState([])
-
+  const [user, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users"); 
+  
   useEffect(() => {
-
-
     const getUsers = async () => {
-      
+      const data = await
+
     }
   }, [])
 
