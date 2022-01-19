b8132679e318a40d2059122bb2ee3c4c0247a5ca
Romina <83961643+rominalodolo@users.noreply.github.com>
2022-01-19 16:43:38 +0200

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,14 +3,10 @@ import { getFirestore } from "@firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
 
 // rather create an .env file and populate the strings in that file for safety
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCcG7BGPdKTRxcdK7TMpL0wkHviGU0aAlY",
-  authDomain: "fir-crud-react-3d90b.firebaseapp.com",
-  projectId: "fir-crud-react-3d90b",
-  storageBucket: "fir-crud-react-3d90b.appspot.com",
-  messagingSenderId: "206334012285",
-  appId: "1:206334012285:web:31078e6161b3ae667f7a14",
-  measurementId: "G-Y3VXVT835N",
+  // data has been removed for saftey reasons, fetch api details from firebase data settings 
+
 };
 
 const app = initializeApp(firebaseConfig);
